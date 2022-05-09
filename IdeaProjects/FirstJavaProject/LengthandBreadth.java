0fab0b01c37cff409e5a66d46c715fdb9397ed21
sunithaam <sunitha.achar@gmail.com>
2022-05-08 22:33:13 -0700

    Merge pull request #2 from sunithaachar/sunitha.achar
    [CACLE-102]-Changed names of User as per my convinience

---
+++ IdeaProjects/FirstJavaProject/LengthandBreadth.java
@@ -0,0 +1,6 @@
+public class LengthandBreadth {
+    public static void main(String[] args) {
+        Scanner scn = new scanner(system.in);
+        System.out.println("Enter Length and Breadth of a Rectangle");
+    }
+}
